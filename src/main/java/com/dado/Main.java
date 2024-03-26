ed0f803e820de2159ae0fef5c9db90fb2a086f6c
dadoChou <dadochou@fareastone.com.tw>
2024-03-26 16:20:31 +0800

    Add new sout message

--- src/main/java/com/dado/Main.java
+++ src/main/java/com/dado/Main.java
@@ -3,5 +3,6 @@ package com.dado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Test Sync origin-lab-it!");
     }
 }
